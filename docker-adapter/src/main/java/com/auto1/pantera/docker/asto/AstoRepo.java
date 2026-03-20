3a1b7fe75b3a76c3aad0db27c55788c546f94a5d
Ayd Asraf <ayd.asraf@auto1.com>
2026-03-20 14:20:59 +0100

    refactor: apply GPLv3/Auto1 license headers to all source files

--- docker-adapter/src/main/java/com/auto1/pantera/docker/asto/AstoRepo.java
+++ docker-adapter/src/main/java/com/auto1/pantera/docker/asto/AstoRepo.java
@@ -1,8 +1,13 @@
 /*
- * The MIT License (MIT) Copyright (c) 2020-2023 pantera.com
- * https://github.com/pantera/pantera/blob/master/LICENSE.txt
+ * Copyright (c) 2025-2026 Auto1 Group
+ * Maintainers: Auto1 DevOps Team
+ * Lead Maintainer: Ayd Asraf
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3.0.
+ *
+ * Originally based on Artipie (https://github.com/artipie/artipie), MIT License.
  */
-
 package com.auto1.pantera.docker.asto;
 
 import com.auto1.pantera.asto.Storage;
